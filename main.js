985ed02e1be84cf04973ba58d8f8bead6bc04fe4
seanwsutter <74794351+seanwsutter@users.noreply.github.com>
2023-07-05 16:08:31 -0500

    js

--- main.js
+++ main.js
@@ -2,7 +2,37 @@ console.log('javascript practice');
 // in-line comment
 /* multi-line
 comment */
+
+
+let form = document.getElementById("form");
+let input = document.getElementById("input");
+let msg = document.getElementById("msg");
+let posts = document.getElementById("posts");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("button clicked"); // log
+
+  formValidation();
+});
+
+
+function formValidation() { }
+
+let formValidation = () => {
+  if (input.value === "") {
+    msg.innerHTML = "Post cannot be blank";
+    console.log("failure"); // log
+  } else {
+    console.log("successs"); // log
+    msg.innerHTML = "";
+  }
+};
 /*
+
+
+
+
 -------- NOTES -----------
 8 data types: undefined, null, boolean, string, symbol, bigint, number, and object
 
